9dbe9d375b47bbf148bde72903fd1f333af3c0f7
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-11-05 20:10:46 +0700

    y

--- webAI.py
+++ webAI.py
@@ -1,25 +1,70 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
 st.title("""facebook AI""")
-st.write("""my hello""")
 
-x_value = st.slider("Select x value", 0.0, 1.0, 0.5)  # default value set to 0.5
-y_value = st.slider("Select y value", 0.0, 1.0, 0.5)  # default value set to 0.5
+col1, col2 = st.columns(2)
+with col1:
+    x_value = st.slider("Select x value", 0.0, 10.0, 5.0, step=0.0001)
 
+with col2:
+    x_value = st.number_input("Insert x value", value=x_value)
+
+with col1:
+    y_value = st.slider("Select y value", 0.0, 10.0, 5.0, step=0.0001)
+
+with col2:
+    y_value = st.number_input("Insert y value", value=y_value)
+
+cold1, cold2, cold3 = st.columns(3)
+with cold1:
+    dateinput = st.selectbox(
+        "select day",
+        ("sunday","monday","tuesday","wednesday","thursday","friday","saturday"),
+    )
+with cold2:
+    monthinput = st.selectbox(
+        "select month",
+        (1,2,3,4,5,6,7,8,9,10,11,12),
+    )
+with cold3:
+    hourinput = st.number_input("Insert hour", min_value=0, max_value=23, step=1, format="%d")
+
+ 
+monthinput-=1
+
+cols1, cols2, cols3 = st.columns(3)
 chart_data = pd.DataFrame({"x": [x_value], "y": [y_value]})
-
-# Scatter plot
-st.write("Scatter Plot with Single Point")
-st.write(chart_data)
+with cols1:
+    st.write(chart_data)
+with cols2:
+    st.write(dateinput,monthinput,hourinput)
 st.vega_lite_chart(chart_data, {
     'width': 700, 
     'height': 600, 
     'mark': {'type': 'point', 'size': 100},
     'encoding': {
-        'x': {'field': 'x', 'type': 'quantitative', 'scale': {'domain': [0, 1]}},
-        'y': {'field': 'y', 'type': 'quantitative', 'scale': {'domain': [0, 1]}}
+        'x': {'field': 'x', 'type': 'quantitative', 'scale': {'domain': [0, 10]}},
+        'y': {'field': 'y', 'type': 'quantitative', 'scale': {'domain': [0, 10]}}
     }
 })
 
+match dateinput:
+    case "sunday":
+        dayinput=0 
+    case "monday":
+        dayinput=1 
+    case "tuesday":
+        dayinput=2 
+    case "wednesday":
+        dayinput=3 
+    case "thursday":
+        dayinput=4 
+    case "friday":
+        dayinput=5 
+    case "saturday":
+        dayinput=6
+    
 
+st.write("Location :")
